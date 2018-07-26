cdf3c9e7cac65d5538b2d4129e9576b0f5d831dc
Olsi Seferi <oseferi30@gmail.com>
2018-07-26 09:55:59 +0200

    Custom Exception Classes

---
+++ certification/src/main/java/com/ikubinfo/certification/exception/FullNameExistsException.java
@@ -0,0 +1,22 @@
+package com.ikubinfo.certification.exception;
+
+public class FullNameExistsException extends Exception{
+
+	private static final long serialVersionUID = -1528193588252153579L;
+
+	public FullNameExistsException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public FullNameExistsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FullNameExistsException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
---
+++ certification/src/main/java/com/ikubinfo/certification/exception/UsernameExistsException.java
@@ -0,0 +1,24 @@
+package com.ikubinfo.certification.exception;
+
+public class UsernameExistsException extends Exception{
+
+	private static final long serialVersionUID = -4597743312248181313L;
+
+	public UsernameExistsException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public UsernameExistsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UsernameExistsException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
